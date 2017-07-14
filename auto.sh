a2bf7dd976db06f0268fd543e00ba7602e08de2a
Praneeth Mendu <praneethmendu@gmail.com>
2017-07-14 09:53:03 +0530

    auto-commit

--- auto.sh
+++ auto.sh
@@ -5,7 +5,6 @@ do
     if [[ $(git status | diff utd -) ]]; then
         git add .
         git commit -m auto-commit
-        git remote set-url origin https://throwaway951:rugrats1@github.com/throwaway951/parameter_sync.git
         git push
     else
         echo all cool
