d356aa64afe835cae703edaccf1540693b12cc9d
Praneeth Mendu <praneethmendu@gmail.com>
2017-07-14 06:46:38 +0530

    auto-commit

--- auto.sh
+++ auto.sh
@@ -1,4 +1,10 @@
-git add .
-git commit -m auto-commit
-git remote set-url origin https://throwaway951:rugrats1@github.com/throwaway951/parameter_sync.git
-git push
+#!/bin/bash
+
+if [[ $(git status | diff utd -) ]]; then
+    git add .
+    git commit -m auto-commit
+    git remote set-url origin https://throwaway951:rugrats1@github.com/throwaway951/parameter_sync.git
+    git push
+else
+    echo all cool
+fi
